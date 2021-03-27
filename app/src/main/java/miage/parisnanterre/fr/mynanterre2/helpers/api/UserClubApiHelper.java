5ef3a8ead10b7aff40d684fd1b82f55fe16f0fcd
assane-sakho <assanesakho.pro@gmail.com>
2021-03-27 22:59:32 +0100

    [UPDATE] - User clubs publications

--- app/src/main/java/miage/parisnanterre/fr/mynanterre2/helpers/api/UserClubApiHelper.java
+++ app/src/main/java/miage/parisnanterre/fr/mynanterre2/helpers/api/UserClubApiHelper.java
@@ -7,10 +7,17 @@ import androidx.annotation.RequiresApi;
 import com.google.gson.JsonArray;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
+import miage.parisnanterre.fr.mynanterre2.api.club.Publication;
 import miage.parisnanterre.fr.mynanterre2.api.club.SimpleClub;
 import miage.parisnanterre.fr.mynanterre2.api.user.User;
 import miage.parisnanterre.fr.mynanterre2.api.user.UserClub;
@@ -20,9 +27,11 @@ public class UserClubApiHelper extends ApiHelper<UserClub, UserClub> {
 
     private static UserClubApiHelper instance;
     private static String baseEndPoint = "users_club";
+    private ClubPublicationApiHelper clubPublicationApiHelper;
 
     private UserClubApiHelper() {
         super(baseEndPoint, true);
+        clubPublicationApiHelper = ClubPublicationApiHelper.getInstance();
     }
 
     public static UserClubApiHelper getInstance()
@@ -58,5 +67,36 @@ public class UserClubApiHelper extends ApiHelper<UserClub, UserClub> {
         String jsonString = gson.toJson(userClub);
         sendData(jsonString, ApiRequestMethod.DELETE, Optional.of(userClub.getId()));
         userClub.getUser().removeFollowedClub(userClub);
+    }
+
+    private class ElementsCallable implements Callable<List<Publication>> {
+        private final int clubId;
+
+        public ElementsCallable(int clubId) {
+            this.clubId = clubId;
+        }
+
+        public List<Publication> call() throws ExecutionException, InterruptedException {
+            return clubPublicationApiHelper.getAllPublications(clubId);
+        }
+    }
+
+    public List<Publication> getFollowedClubsPublication(User user) throws InterruptedException, ExecutionException {
+        List<Publication> publications = new ArrayList<>();
+
+        ExecutorService executorService = Executors.newFixedThreadPool(user.getFollowedClubsIds().size());
+
+        List<ElementsCallable> callableTasks = new ArrayList<>();
+
+        for (int clubId: user.getFollowedClubsIds()) {
+            callableTasks.add(new ElementsCallable(clubId));
+        }
+
+        List<Future<List<Publication>>> futures = executorService.invokeAll(callableTasks);
+
+        for (Future<List<Publication>> f : futures)
+            publications.addAll(f.get());
+
+        return publications;
     }
 }
